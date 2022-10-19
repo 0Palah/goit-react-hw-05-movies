4d1c48f0c6bc0832755dcdc6ac2012a7f7c370eb
Oleg Palashynskyi <palah@ukr.net>
2022-10-19 19:27:25 +0300

    Update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <p>Page not found</p>
+      <Link to={`/`}>Back to homepage</Link>
+    </div>
+  );
 };
 
 export default NotFound;
